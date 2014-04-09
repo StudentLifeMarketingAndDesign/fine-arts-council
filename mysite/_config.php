8539754ecb4794fdd081503b54986792a2030d57
Dustin Quam <quamsta@gmail.com>
2014-04-09 11:09:14 -0500

    updated config

---
+++ mysite/_config.php
@@ -0,0 +1,23 @@
+<?php
+global $project;
+$project = 'mysite';
+
+global $database;
+$database = 'finearts';
+
+require_once("conf/ConfigureFromEnv.php");
+global $databaseConfig;
+
+MySQLDatabase::set_connection_charset('utf8');
+
+// This line set's the current theme. More themes can be
+// downloaded from http://www.silverstripe.org/themes/
+SSViewer::set_theme('blackcandy');
+
+// Set the site locale
+i18n::set_locale('en_US');
+
+// enable nested URLs for this site (e.g. page/sub-page/)
+SiteTree::enable_nested_urls();
+Director::set_environment_type("live");
+
