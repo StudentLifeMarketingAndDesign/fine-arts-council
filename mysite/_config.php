c3bda84b34390601fc2eb27ffb7d2cdb953d34ea
Dustin Quam <quamsta@gmail.com>
2017-11-13 10:01:59 -0600

    updated SS + modules

--- mysite/_config.php
+++ mysite/_config.php
@@ -20,6 +20,7 @@ i18n::set_locale('en_US');
 // enable nested URLs for this site (e.g. page/sub-page/)
 SiteTree::enable_nested_urls();
 
+Authenticator::unregister('MemberAuthenticator');
 Authenticator::set_default_authenticator('SAMLAuthenticator');
 if(Director::isLive()) {
 	Director::forceSSL();
