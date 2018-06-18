f746cc954a28801454803da2bd35f561f89eb3de
Dustin Quam <quamsta@gmail.com>
2018-06-18 13:06:03 -0500

    initial port to division project

--- mysite/_config.php
+++ mysite/_config.php
@@ -1,30 +1,27 @@
 <?php
+
 global $project;
 $project = 'mysite';
 
 global $database;
+//!!!!!!!Define your database name:
 $database = 'finearts';
-
+ 
+// Use _ss_environment.php file for configuration
 require_once("conf/ConfigureFromEnv.php");
-global $databaseConfig;
 
 MySQLDatabase::set_connection_charset('utf8');
 
-// This line set's the current theme. More themes can be
-// downloaded from http://www.silverstripe.org/themes/
-SSViewer::set_theme('blackcandy');
-
 // Set the site locale
 i18n::set_locale('en_US');
-
-// enable nested URLs for this site (e.g. page/sub-page/)
-SiteTree::enable_nested_urls();
-
-Authenticator::unregister('MemberAuthenticator');
-Authenticator::set_default_authenticator('SAMLAuthenticator');
-
-Email::set_mailer( new SmtpMailer() );
+FulltextSearchable::enable();
+// Enable nested URLs for this site (e.g. page/sub-page/)
+if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
 
 if(Director::isLive()) {
 	Director::forceSSL();
 }
+Authenticator::unregister('MemberAuthenticator');
+Authenticator::set_default_authenticator('SAMLAuthenticator');
+
+SSViewer::set_theme('cfo-subtheme');
