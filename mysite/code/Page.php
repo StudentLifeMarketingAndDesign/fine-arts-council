5c5f9195929ff13f5e55b5ac5d9156767f529513
Dustin Quam <quamsta@gmail.com>
2018-06-19 13:50:48 -0500

    cfo subtheme working

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -1,56 +1,10 @@
 <?php
-class Page extends SiteTree {
 
-	private static $db = array(
-	);
+use SilverStripe\CMS\Model\SiteTree;
 
-	private static $has_one = array(
-	);
+class Page extends SiteTree
+{
+    private static $db = [];
 
-}
-class Page_Controller extends ContentController {
-
-	/**
-	 * An array of actions that can be accessed via a request. Each array element should be an action name, and the
-	 * permissions or conditions required to allow the user to access it.
-	 *
-	 * <code>
-	 * array (
-	 *     'action', // anyone can access this action
-	 *     'action' => true, // same as above
-	 *     'action' => 'ADMIN', // you must have ADMIN permissions to access this action
-	 *     'action' => '->checkAction' // you can only access this action if $this->checkAction() returns true
-	 * );
-	 * </code>
-	 *
-	 * @var array
-	 */
-	private static $allowed_actions = array(
-	);
-
-	public function init() {
-		parent::init();
-
-		// Note: you should use SS template require tags inside your templates
-		// instead of putting Requirements calls here.  However these are
-		// included so that our older themes still work
-		Requirements::themedCSS('layout');
-		//	Requirements::themedCSS('typography');
-		Requirements::themedCSS('form');
-	}
-
-	public function MarketTheme() {
-
-		$month    = date('n');
-		$monthInt = intval($month);
-
-		//If we're between January and September, return "spring"
-		if (($monthInt >= 1) && ($monthInt < 9)) {
-			return "spring";
-		} else {
-			return "winter";
-		}
-
-	}
-
+    private static $has_one = [];
 }
