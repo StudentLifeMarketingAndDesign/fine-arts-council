4928dcf0a86a8a5e9d21e105b56c18cd66773b81
Dustin Quam <quamsta@gmail.com>
2018-08-08 08:36:02 -0500

    use myisam for page tables

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -1,10 +1,15 @@
 <?php
 
 use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\ORM\Connect\MySQLSchemaManager;
 
 class Page extends SiteTree
 {
     private static $db = [];
 
     private static $has_one = [];
+
+    private static $create_table_options = [
+        MySQLSchemaManager::ID => 'ENGINE=MyISAM'
+    ];
 }
