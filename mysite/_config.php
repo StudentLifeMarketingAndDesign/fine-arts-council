c86e82ef1ecd3e43fc4aaf95d0a142326791d8dc
Dustin Quam <quamsta@gmail.com>
2018-08-08 13:28:38 -0500

    remove duplicate fulltextsearchable enabler

--- mysite/_config.php
+++ mysite/_config.php
@@ -2,11 +2,10 @@
 
 use SilverStripe\Security\PasswordValidator;
 use SilverStripe\Security\Member;
-use SilverStripe\ORM\Search\FulltextSearchable;
+
 // remove PasswordValidator for SilverStripe 5.0
 $validator = new PasswordValidator();
 
 $validator->minLength(8);
 $validator->checkHistoricalPasswords(6);
 Member::set_password_validator($validator);
-FulltextSearchable::enable();
